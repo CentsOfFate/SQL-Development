b1ef870c56cae78472041b6ad597e4dd2312988a
CentsOfFate <charlie.dalldorf@gmail.com>
2022-10-14 10:10:02 -0500

    Add Column.

--- Tables/dbo.Customer.sql
+++ Tables/dbo.Customer.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Customer]
 (
 [Id] [int] NULL,
-[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AccountNumber] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
